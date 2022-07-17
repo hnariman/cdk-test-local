6ea148f55cc93ea96d61cb149b5cf9e1c3ccc52f
Nariman Huseynov <hnariman@gmail.com>
2022-07-18 03:32:27 +0400

    some minor fixes

--- bin/test.ts
+++ bin/test.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { TutorialStack } from '../lib/tutorial-stack';
+import { TestStack } from '../lib/test-stack';
 
 const app = new cdk.App();
-new TutorialStack(app, 'TutorialStack', {
+new TestStack(app, 'TestStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
